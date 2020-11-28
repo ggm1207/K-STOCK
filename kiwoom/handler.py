3ff7122dacc77f29f25cc09eeec6e19b11b58a2a
Goo Gunmo <ggm1207@gmail.com>
2020-11-28 21:36:08 +0900

    update

--- kiwoom/handler.py
+++ kiwoom/handler.py
@@ -1,10 +1,12 @@
 from PyQt5.QtCore import QEventLoop
 from PyQt5.QAxContainer import QAxWidget
 
+
 class Kiwoom(QAxWidget):
     """Kiwoom API
     server - 1: 모의서버, 나머지: 실서버
     """
+
     def __init__(self):
         super().__init__()
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
@@ -13,7 +15,9 @@ class Kiwoom(QAxWidget):
         self.login_event_loop = QEventLoop()
         self.login_event_loop.exec_()
 
-        self.server = self.dynamicCall("KOA_Functions(QString, QString)", "GetServerGubun", "")
+        self.server = self.dynamicCall(
+            "KOA_Functions(QString, QString)", "GetServerGubun", ""
+        )
 
     def _event_connect(self, err_code):
         if err_code == 0:
@@ -22,6 +26,10 @@ class Kiwoom(QAxWidget):
             print("disconnected")
 
         self.login_event_loop.exit()
-        
+
+
 class Handler(QAxWidget):
-    kiwoom = Kiwoom()
+    kiwoom = None
+
+    def __init__(self):
+        self.kiwoom = Kiwoom()
