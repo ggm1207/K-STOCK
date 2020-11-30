cf2b245f3578b9c72ee3d12c98da156cb8892253
Goo Gunmo <ggm1207@gmail.com>
2020-11-30 21:06:11 +0900

    update

--- app.py
+++ app.py
@@ -57,6 +57,9 @@ class App(QMainWindow, UI):
         """조회요청 응답을 받거나 조회데이터를 수신했을때 호출됩니다.
         조회데이터는 이 이벤트내부에서 GetCommData()함수를 이용해서
         얻어올 수 있습니다."""
+
+        print("Helper, trcode: {} rcname: {}".format(sTrCode, sRecordName))
+
         self.helper.trcode = sTrCode
         self.helper.rcname = sRecordName
         self.helper.block.exit()  # TODO: 비동기에서는...?
